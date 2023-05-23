aacc5ddff0b679588fc9ec85c1be97f1b0b75263
sherry-K <131955198+sherry-K@users.noreply.github.com>
2023-05-24 01:39:21 +0900

    help：合体させましたがボタンが機能してません（ゲームオーバー画面はとりあえずコメントアウト）

--- sketch.js
+++ sketch.js
@@ -1,7 +1,6 @@
 //----エンティティ関連の関数 ----------------------------------------------
 
 
-
 // プレイヤーエンティティ
 function createPlayer(){
   return {
@@ -19,7 +18,7 @@ function createPlayer(){
 }
 
 function drawplayer(player){
-  image(img,player.x - 40, player.y - 10, 80, 80);
+  image(img, player.x - 40, player.y - 10, 80, 80);
 }
 
 
@@ -31,7 +30,7 @@ function createEnemy(){
     lv_i: 1,
     lv_h: 2,
     type: "Human",
-    Name: "socialEconomics",
+    Name: "Social Economics",
     attack: 10,
     diffence: 10,
     x: 600,
@@ -40,37 +39,30 @@ function createEnemy(){
 }
 
 function drawEnemy(enemy){
-  image(img2,enemy.x-30, enemy.y-10, 80, 80);
+  image(img2, enemy.x - 30, enemy.y - 10, 80, 80);
 }
 
+
 // ボタンエンティティ
-function createButtons(button_name, button_x, button_y){
+function createButtons(button_name, button_x, button_y, button_w, button_h){
   return{
     name: button_name,
     x: button_x,
-    y: button_y
+    y: button_y,
+    w: button_w,
+    h: button_h
   }
 }
 
 function drawButton(button){
   rectMode(RADIUS);
   fill(0);
-  rect(button.x, button.y, 70, 30, 20);
+  rect(button.x, button.y, button.w, button.h, 20);
   textSize(30);
-  textAlign(CENTER, CENTER);
   fill(220);
   text(button.name, button.x, button.y);
-  if(mouseIsPressed){
-    if(mouseX >= 80 && mouseX <= 280 && mouseY >= 460 && mouseY <=540){
-      button_status = button.name;
-      ButtonIsPushed();
-    }
-    /*if(mouseX >= 300 && mouseX <= 500 && mouseY >= 460 && mouseY <=540){
-      button_status = button.name;
-      ButtonIsPushed();
-    }*/
-  }
 }
+
 
 // 攻撃エフェクトエンティティ
 function createAttack(){
@@ -85,15 +77,13 @@ function drawAttack(entity){
   if(num <= 1 && game_status == "attack"){
     if(entity.x > enemy.x){
       game_status = "select";
+      // game_status = "gameover";
     } else if(entity.x <= enemy.x) {
       if(attacks == social_attack){
-        image(img4,entity.x, entity.y, 50,50);
+        image(img4, entity.x, entity.y, 50, 50);
+      } else if(attacks == informatic_attack){
+        image(img4, entity.x, entity.y, 75, 75);
       }
-      /*
-      if(attacks == database_attack){
-        circle(entity.x, entity.y, 50);
-      }
-      */
     }
   } else {
     game_status = "select";
@@ -112,91 +102,142 @@ function resetAttackPosition(entity){
   entity.x = player.x;
 }
 
+
+
+
 //----ゲーム全体に関わる部分 ----------------------------------------------
+
+
 /** プレイヤーエンティティ */
 let player;
+
 
 /** エネミーエンティティ */
 let enemy;
 
+
 /** ボタンエンティティ */
+// 社会ボタン
 let social_button;
-//let database_button;
+// 情報ボタン
+let informatic_button;
+// 人間ボタン
+// let human_button;
+
+//タイトル画面のスタートボタン
+let start_button;
+
 
 /**画像 */
-let img;
-let img2;
-let img3;
-let img4;
-let img5;
+let img; // 勇者
+let img2; // 緑の開いた本
+let img3; // 草原
+let img4; // 雷
+let img5; // muramon
+
 
 /**音 */
-let soundFile;
+let soundFile; // おとぼけダンス.mp3
+
 
 // ボタンのステータス
 let button_status;
-// "社会"などが入り、攻撃エフェクトと関連している
+// "Social","Informatic","Human"などが入り、攻撃エフェクトと関連している
 
+
+/** 攻撃 */
 // 攻撃エフェクト
 let attacks;
-// social_attack などが入る
-// 後々、攻撃が複数になった場合、攻撃名のリストになるかも？
+// social_attack, informatic_attack, human_attack が入る
 
-// 社会攻撃エフェクト
-let social_attack;
+let social_attack; // 社会攻撃エフェクト
+let informatic_attack; // 情報攻撃エフェクト
+let human_attack; // 人間攻撃エフェクト
+
 
 // ゲーム状態
 let game_status;
-//今のところ "attack" 攻撃中 か "select" 選択中 が入る
+/**
+ * 今のところ"title"（タイトル画面）か "select"(選択中) か 
+ * "attack" (プレイヤーの攻撃中) か "eAttack"(エネミーの攻撃中) か
+ * "gameover"(プレイヤーが死んだ) か "clear"(エネミーを倒した) が入る
+ */
+
 
 // マウスが押された回数
 let num;
 
-//タイトル画面のボタンエンティティ
-let startButton;
 
 /**タイトル画面の初期化 */
 function resetTitleScreen(){
-  startButton = createButtons("Start", width / 2 , height / 2 + 100);
+  start_button = createButtons("Start", width / 2 , height / 2 + 100, 70, 30);
 }
 
 /**タイトル画面の描画 */
 function drawTitleScreen(){
   textSize(100);
-  textAlign(CENTER, CENTER);
   fill(0);
   image(img5, width / 2 -250, height / 2 - 150, 500, 200);
 
   //スタートボタンの描画
-  drawButton(startButton);
+  drawButton(start_button);
 }
+
+
+/** ゲームオーバー画面の描画 */
+/* function drawGameoverScreen() {
+  background(0, 192);  // 透明度 192 の黒
+  fill(255);
+  strokeWeight(5);
+  stroke(0, 100, 250);
+  textSize(64);
+  text("You lose...", width / 2, height / 2);  // 画面中央にテキスト表示
+  textSize(25);
+  noStroke();
+  text("Click to retry", width / 2, height / 2 + 100);  // 画面中央にテキスト表示
+}*/
+
 
 /** ステータスの描画 */
 function drawStatus(entity) {
   textSize(25);
-  textAlign(CENTER, CENTER);
   fill(0);
-  text('HP:' + entity.hp, entity.x, 200);
-  text('Name:' + entity.Name, entity.x, 220);
-  //text('社会Lv:' + entity.lv_s + ' ' + '情報Lv:' + entity.lv_i + ' ' + '人間Lv:' + entity.lv_h, entity.x, 210);
+  text("HP:" + entity.hp, entity.x, 200);
+  text(entity.Name, entity.x, 230);
+  //text("社会Lv:" + entity.lv_s + " " + "情報Lv:" + entity.lv_i + " " + "人間Lv:" + entity.lv_h, entity.x, 210);
 
-  //text(social_attack.x + "  " + enemy.x, 200, 375);
-  //text(database_attack.x + "  " + enemy.x, 200, 400);
-  //text(game_status, 200, 425);
+
+  // 以下、攻撃エフェクト確認用
+  // text("社 " + social_attack.x + "  " + enemy.x, 200, 375);
+  // text("情 " + informatic_attack.x + "  " + enemy.x, 200, 400);
+  // text(game_status, 200, 425);
 }
 
 /** ウィンドウの描画 */
 function drawWindow(){
   if(game_status == "attack") { 
     textSize(30);
-    textAlign(CENTER, CENTER);
     fill(0);
-    text("Player's " + button_status + ' attack!', 400, 50);
+    text("Player's " + button_status + " attack!", 400, 50);
+  } else if(game_status == "eAttack") {
+    game_status = "eAttack";
+    fill(0);
+    noStroke();
+    textSize(20);
+    text("Enemy's attack! Player recieved " + enemy.attack + " damege!", 400, 50);
+  } else if (game_status == "select"){
+    fill(0);
+    noStroke();
+    textSize(20);
+    text("Select your attack", 400, 50);
   }
 }
 
 /** ゲームの初期化 */
 function resetGame(){
+  // ゲーム状態の初期化
+  game_status = "title";
+
   //ゲームの初期化時にタイトル画面も初期化する
   resetTitleScreen();
 
@@ -207,17 +248,14 @@ function resetGame(){
   enemy = createEnemy();
 
   // ボタンの生成
-  social_button = createButtons("Social", 180, 500);
-  //database_button = createButtons("データベース基礎", 400, 500);
+  social_button = createButtons("Social", 180, 500, 70, 30);
+  informatic_button = createButtons("Informatic", 400, 500, 100, 30);
 
   // 社会攻撃エフェクトの生成
   social_attack = createAttack();
 
-  // データベース基礎攻撃エフェクトの生成
-  //database_attack = createAttack();
-
-  // ゲーム状態の初期化
-  game_status = "title";
+  // 情報攻撃エフェクトの生成
+  informatic_attack = createAttack();
 
   //マウスが押された回数numの初期化
   num = 0;
@@ -226,44 +264,44 @@ function resetGame(){
 /** ゲームの更新*/
 function updateGame(){
   updateAttackPosition(social_attack);
-  //updateAttackPosition(database_attack);
+  updateAttackPosition(informatic_attack);
+
+  // プレイヤーのHPを減らす
+  updatePlayerHp();
+
+  // プレイヤーが死んでいたらゲームオーバー状態にする
+  // if (player.hp==0) game_status = "gameover";
 }
 
 /** ゲームの描画 */
 function drawGame(){
   if(game_status === "title"){
     drawTitleScreen();
-  }
-  else{
+  /* } else if (game_status === "gameover"){
+    drawGameoverScreen();
+    return; */
+  } else {
   drawplayer(player);
   drawEnemy(enemy);
 
-// ボタンの描画
-  drawButton(social_button);
-  //drawButton(database_button);
+  // ボタンの描画
+  drawButton(social_button);  // 社会ボタン描画
+  drawButton(informatic_button);  // 情報ボタン描画
 
-// ステータスの描画
+  // ステータスの描画
   drawStatus(player);
   drawStatus(enemy);
 
-// 攻撃エフェクトの描画
-  drawAttack(social_attack);
+  // 攻撃エフェクトの描画
+  drawAttack(social_attack); // 社会攻撃エフェクト描画
+  drawAttack(informatic_attack); // 情報攻撃エフェクト描画
 
-// ウィンドウの描画
+  // エネミーの攻撃エフェクトの描画
+  //(あとで)
+
+  // ウィンドウの描画
   drawWindow();
   }
-}
-
-/** ボタンが押されたら */
-function ButtonIsPushed(){
-  if(button_status == "Social"){
-    attacks = social_attack;
-  }
-  /*
-  if(button_status == "データベース基礎"){
-    attacks = database_attack;
-  }
-  */
 }
 
 // HPの更新
@@ -281,25 +319,26 @@ function countHpIsZero(){
   }
 }
 
-//敵の攻撃
-function enemyAttack(){
-  for(let i=0; i<1; i++){
-    if(i==0){
-      player.hp-=enemy.attack;
+// プレイヤーのHPの更新
+function updatePlayerHp(){
+  if(game_status == "eAttack"){
+    for(let i=0; i<1; i++){
+      if(i==0){
+        player.hp-=enemy.attack;
+      }
     }
   }
-
 }
 
 //----setup/draw 他 ------------------------------------------------------
 
 function preload(){
-  img = loadImage('./勇者.png');
-  img2 = loadImage('./緑の開いた本.png');
-  img3 = loadImage('./草原.png');
-  img4 = loadImage('./雷.png');
-  img5 = loadImage('./muramon.png');
-  soundFile = createAudio('おとぼけダンス.mp3');
+  img = loadImage("./勇者.png");
+  img2 = loadImage("./緑の開いた本.png");
+  img3 = loadImage("./草原.png");
+  img4 = loadImage("./雷.png");
+  img5 = loadImage("./muramon.png");
+  soundFile = createAudio("おとぼけダンス.mp3");
 }
 
 function setup() {
@@ -307,11 +346,13 @@ function setup() {
   image(img3, 0, 0, 800, 600); // 800 x 600 ピクセル
   textFont("VT323");
   rectMode(CENTER); //四角形の基準点を中心に変更
+  textAlign(CENTER, CENTER);
   background(220);
 
   game_status = "title";
-  resetGame();
   soundFile.loop();
+  
+  resetGame();
 }
 
 function draw() {
@@ -321,37 +362,47 @@ function draw() {
 }
 
 function mousePressed(){
-  if(game_status == "select" && mouseX >= 80 && mouseX <= 280 && mouseY >= 460 && mouseY <=540){
+  if(game_status == "select"){
     resetAttackPosition(social_attack);
+    resetAttackPosition(informatic_attack);
+    //resetAttackPosition(human_attack);
+    if(
+      mouseY >= 500 - (30 / 2) &&
+      mouseY <= 500 + (30 / 2)){
+        if( // 社会ボタンが押されたら
+        mouseX >= social_button.x - (social_button.w / 2) &&
+        mouseX <= social_button.x + (social_button.w / 2) 
+        ){
+          button_status = "Social";
+          attacks = social_attack;
+        } else if( // 情報ボタンが押されたら
+        mouseX >= informatic_button.x - (informatic_button.w / 2) &&
+        mouseX <= informatic_button.x + (informatic_button.w / 2)
+        ){
+          button_status = "Informatic";
+          attacks = informatic_attack;
+        } 
+      }
+
     game_status = "attack";
     updateHp();
     countHpIsZero();
-    enemyAttack();
   }
 
   if (game_status === "title") {
-    // スタートボタンが押された場合
-    if (
-      mouseX >= startButton.x - 35 &&
-      mouseX <= startButton.x + 35 &&
-      mouseY >= startButton.y - 15 &&
-      mouseY <= startButton.y + 15
-    ) {
+    if( // スタートボタンが押されたら
+      mouseX >= start_button.x - (start_button.w / 2) &&
+      mouseX <= start_button.x + (start_button.w / 2) &&
+      mouseY >= start_button.y - (start_button.h / 2) &&
+      mouseY <= start_button.y + (start_button.h / 2)
+    ){
       game_status = "select";
       // 以降のゲームの進行コード...
     }
   }
-  /*
-  if(mouseX >= 300 && mouseX <= 500 && mouseY >= 460 && mouseY <=540){
-    resetAttackPosition(database_attack);
-    game_status = "attack";
-    updateHp();
-    countHpIsZero();
-  }
-  */
-}
 
-function changeBG(){
-  let val = random(255);
-  background(val);
+  /* if(game_status == "gameover"){
+    // ゲームオーバー状態ならリセット
+    resetGame();
+  } */
 }
